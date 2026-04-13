7b00c170b23d0986d891f0d16701a3154afec04e
Rashid Enahora <rashidenahora@MacBookPro.attlocal.net>
2026-04-13 01:43:44 -0400

    Replaced imports with import alias for screensaver exercise

--- src/app/exercises/01-screensaver/[color]/page.js
+++ src/app/exercises/01-screensaver/[color]/page.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import ScreenSaver from '../../../../components/ScreenSaver';
+import ScreenSaver from '@/components/ScreenSaver';
 
 async function ScreenSaverExercise({ params }) {
     const { color } = await params;
--- src/app/exercises/02-flash-messages/page.js
+++ src/app/exercises/02-flash-messages/page.js
@@ -1,3 +1,4 @@
+// Server Component
 import React from 'react';
 import Link from 'next/link';
 
